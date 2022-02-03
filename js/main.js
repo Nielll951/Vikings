8c19608f246d30361f439b8c8701b9cddf32ca8b
Kostya <rodina9180gaocheng@ukr.net>
2022-02-03 19:02:38 +0200

    adapted some elements

--- js/main.js
+++ js/main.js
@@ -6,6 +6,15 @@ $(function(){
       asNavFor: '.heroes__slider-text',
       prevArrow: '<button class="slick-btn slick-prev"><img src="images/arrow-left.png" alt="prev"></button>',
       nextArrow: '<button class="slick-btn slick-next"><img src="images/arrow-right.png" alt="next"></button>',
+      responsive: [
+         {
+           breakpoint: 769,
+           settings: {
+             arrows: false,
+             dots: true
+           }
+         },
+      ]
    });
    $('.heroes__slider-text').slick({
       slidesToShow: 1,
