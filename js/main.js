a91421137886f148842853d3b8bf264307b28501
Kostya <rodina9180gaocheng@ukr.net>
2022-02-05 17:01:35 +0200

    completed adaptivity

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 $(function(){
 
+   $('.menu__btn').on('click', function() {
+      $('.menu__list').toggleClass('menu__list--active');
+   });
+
    $('.heroes__slider-img').slick({
       slidesToShow: 1,
       slidesToScroll: 1,
+      adaptiveHeight: true,
       asNavFor: '.heroes__slider-text',
       prevArrow: '<button class="slick-btn slick-prev"><img src="images/arrow-left.png" alt="prev"></button>',
       nextArrow: '<button class="slick-btn slick-next"><img src="images/arrow-right.png" alt="next"></button>',
